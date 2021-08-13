1fd336d5639c9f68f670e4fb320c60bf226ea889
mtj0928 <mtj0928@users.noreply.github.com>
2021-08-14 00:05:28 +0900

    add apply

--- Sources/DDS/exntensions/View+extension.swift
+++ Sources/DDS/exntensions/View+extension.swift
@@ -3,6 +3,11 @@ import SwiftUI
 extension View {
 
     @ViewBuilder
+    public func apply<V: View>(@ViewBuilder handler: (Self) -> V) -> some View {
+        handler(self)
+    }
+
+    @ViewBuilder
     public func `if`<V: View>(_ condition: Bool, @ViewBuilder handler: (Self) -> V) -> some View {
         if condition {
             handler(self)
