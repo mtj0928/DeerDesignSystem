6a42ffc4317d10566749df2954d44fc3ad3d65e5
mtj0928 <mtj0928@users.noreply.github.com>
2021-08-15 16:00:26 +0900

    add tag to Demo

--- DDSDemo/Shared/TextAndButtonView.swift
+++ DDSDemo/Shared/TextAndButtonView.swift
@@ -17,9 +17,12 @@ struct TextAndButtonView: View {
                         Text("Primary Text Color: Deer / まつじ / 松本")
                             .preferredFont(for: .body)
                             .foregroundColor(DDSColor.primaryText.swiftUIColor)
+                            .padding(.horizontal)
+
                         Text("Secondary Text Color: Deer / まつじ / 松本")
                             .preferredFont(for: .body)
                             .foregroundColor(DDSColor.secondaryText.swiftUIColor)
+                            .padding(.horizontal)
 
                         ZStack {
                             RoundedRectangle(cornerRadius: 10.0)
@@ -38,6 +41,7 @@ struct TextAndButtonView: View {
                             }
                             .padding(14)
                         }
+                        .padding(.horizontal)
 
                         if showTips {
                             Tips(
@@ -51,12 +55,37 @@ struct TextAndButtonView: View {
                                 }
                             )
                             .foregroundColor(DDSColor.deerBlue.swiftUIColor)
+                            .padding(.horizontal)
                         }
+
+                        Text("Tag")
+                            .preferredFont(for: .footnote, weight: .bold)
+                            .foregroundColor(DDSColor.primaryText.swiftUIColor)
+                            .padding(.horizontal)
+
+                        ScrollView(.horizontal) {
+                            HStack {
+                                Tag(title: Text("Tag"), action: { showAlert = true })
+                                    .foregroundColor(DDSColor.deerBlue.swiftUIColor)
+                                Tag(title: Text("Tag"), action: { showAlert = true })
+                                    .foregroundColor(DDSColor.deerRed.swiftUIColor)
+                                Tag(
+                                    title: Text("Tag").foregroundColor(DDSColor.deerBlue.swiftUIColor),
+                                    action: { showAlert = true }
+                                ).foregroundColor(DDSColor.deerBlue.swiftUIColor.opacity(0.15))
+                                Tag(
+                                    title: Text(Image(systemSymbol: .plus)),
+                                    shape: Circle(),
+                                    action: { showAlert = true }
+                                ).foregroundColor(DDSColor.secondaryBackground.swiftUIColor)
+                            }
+                            .padding(.horizontal)
+                        }
+
                         Spacer()
                     }
                     Spacer()
                 }
-                .padding(16.0)
             }
             .alert(isPresented: $showAlert) {
                 Alert(title: Text("Alert"))
