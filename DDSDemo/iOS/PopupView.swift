87be92eac7e30fa3bf5795672544e88bef3ce57f
mtj0928 <mtj0928@users.noreply.github.com>
2021-09-27 22:09:55 +0900

    [DEMO] add popup

--- DDSDemo/iOS/PopupView.swift
+++ DDSDemo/iOS/PopupView.swift
@@ -11,24 +11,54 @@ import DeerDesignSystem
 struct PopupView: View {
 
     @Environment(\.popupCenter) var popupCenter
+    @State var flag = false
 
     var body: some View {
-        Button("Show popup") {
-            popupCenter?.popup = Popup(content: {
-                VStack {
-                    Text("Popup")
-                    Spacer()
-                    Button("OK") {
-                        popupCenter?.popup = nil
+        Alignment {
+            Button("Show popup") {
+                popupCenter?.popup = Popup(content: {
+                    VStack(spacing: 0) {
+                        Color.gray
+                            .frame(height: 176)
+                            .cornerRadius(8)
+                            .padding([.top, .horizontal], 32)
+                            .padding(.bottom, 16)
+                        VStack(spacing: 8) {
+                            Text("Title")
+                                .preferredFont(for: .body, weight: .bold)
+                                .foregroundColor(.dds.primaryText)
+                            Text("xxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+                                .preferredFont(for: .footnote, weight: .regular)
+                                .foregroundColor(.dds.primaryText)
+                            Spacer(minLength: 0)
+                            CheckButton(title: "Don’t show this alert again", isSelected: $flag)
+                                .foregroundColor(.dds.secondaryText)
+                            Spacer(minLength: 0)
+                        }
+                        .padding(.bottom, 12)
+                        Button(
+                            action: { popupCenter?.popup = nil },
+                            label: {
+                                HStack {
+                                    Spacer()
+                                    Text("OK")
+                                        .preferredFont(for: .callout, weight: .bold)
+                                        .foregroundColor(.dds.primaryText)
+                                    Spacer()
+                                }
+                            }
+                        )
+                            .padding(.bottom, 24)
                     }
-                }
-                .padding()
-                .frame(width: 200, height: 200)
-                .background(Color.dds.primaryBackground)
-                .cornerRadius(12)
-//                .transition(.scale.animation(.linear.speed(2)))
-            })
+                    .frame(width: 310, height: 377)
+                    .background(Color.dds.popupBackground)
+                    .cornerRadius(29)
+                    .popupTransition()
+                })
+            }
         }
+        .background(Color.dds.primaryBackground)
+        .ignoresSafeArea()
     }
 }
 
