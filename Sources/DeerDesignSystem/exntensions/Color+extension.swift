d0d99134363f58e74af593c61b34f18b72dc57cd
mtj0928 <mtj0928@users.noreply.github.com>
2021-10-12 09:42:43 +0900

    fix module bug

--- Sources/DeerDesignSystem/exntensions/Color+extension.swift
+++ Sources/DeerDesignSystem/exntensions/Color+extension.swift
@@ -4,7 +4,7 @@ extension Color {
     public static var dds: DeerColor.Type { DeerColor.self }
 
     fileprivate static func asset(name: String) -> Color {
-        Color(name, bundle: Bundle.module)
+        Color(name, bundle: Bundle.currentModuel)
     }
 }
 
@@ -24,4 +24,40 @@ public struct DeerColor {
     public static let secondaryBackground = Color.asset(name: "secondary_background")
     public static let secondaryText = Color.asset(name: "secondary_text")
     public static let tagText = Color.asset(name: "tag_text")
+}
+
+
+// MARK: - Workaround for module bug
+
+public let imageBundle = Bundle.currentModuel
+private class CurrentBundleFinder {}
+
+extension Foundation.Bundle {
+
+    static var currentModuel: Bundle = {
+        let bundleName = "DeerDesignSystem_DeerDesignSystem"
+
+        let bundleNameIOS = "LocalPackages_Database"
+        let bundleNameMacOs = "DeerDesignSystem_DeerDesignSystem"
+
+        let candidates = [
+            Bundle.main.resourceURL,
+            Bundle(for: CurrentBundleFinder.self).resourceURL,
+            Bundle.main.bundleURL,
+            Bundle(for: CurrentBundleFinder.self).resourceURL?.deletingLastPathComponent().deletingLastPathComponent().deletingLastPathComponent(),
+            Bundle(for: CurrentBundleFinder.self).resourceURL?.deletingLastPathComponent().deletingLastPathComponent(),
+        ]
+
+
+        for candidate in candidates {
+            let bundlePathiOS = candidate?.appendingPathComponent(bundleNameIOS + ".bundle")
+            let bundlePathMacOS = candidate?.appendingPathComponent(bundleNameMacOs + ".bundle")
+            if let bundle = bundlePathiOS.flatMap(Bundle.init(url:)) {
+                return bundle
+            } else if let bundle = bundlePathMacOS.flatMap(Bundle.init(url:)) {
+                return bundle
+            }
+        }
+        fatalError("unable to find bundle named DeerDesignSystem_DeerDesignSystem")
+    }()
 }
